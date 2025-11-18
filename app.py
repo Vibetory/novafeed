f83a6a6b5f69a91f9a279b0a8dce65ad7eeb54f5
Valentin Bretel <67462878+Vibetory@users.noreply.github.com>
2025-11-18 14:41:43 +0000

    btn search modifié

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 import feedparser
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
 RSS_FEEDS = {
-    'BuildingSmart OpenBIM': 'https://www.buildingsmart.org/feed/',
-    'IEA – Smart Buildings & Energy': 'https://www.iea.org/rss/news',
-    'Smart Buildings Magazine': 'https://smartbuildingsmagazine.com/feed',
-    'IEEE IoT Journal': 'https://ieeexplore.ieee.org/rss/TOC84.XML',
-    '5G Americas': 'https://www.5gamericas.org/feed/',
-    'Ericsson 5G Insights': 'https://www.ericsson.com/api/rss/section/media',
-    'GreenTechMedia': 'https://www.greentechmedia.com/rss',
-    'Architecture 2030 – Climate Action': 'https://architecture2030.org/feed/',
-    'ArchDaily – Smart & Sustainable': 'https://www.archdaily.com/rss',
-    'TechCrunch IoT': 'https://techcrunch.com/tag/iot/feed/',
+    "IEEE IoT Journal": {
+        "url": "https://ieeexplore.ieee.org/rss/TOC84.XML",
+        "theme": "IoT",
+        "ranking": 5,             # qualité de la source
+        "ranking_author": "Valentin"
+    },
+    "TechCrunch IoT": {
+        "url": "https://techcrunch.com/tag/iot/feed/",
+        "theme": "IoT",
+        "ranking": 4,
+        "ranking_author": "Valentin"
+    },
+    # ...
 }
 
 @app.route('/')
 def index():
     articles = []
-    for source, feed in RSS_FEEDS.itmes():
+    for source, feed in RSS_FEEDS.items():
         parsed_feed = feedparser.parse(feed)
         entries = [(source, entry) for entry in parsed_feed.entries]
+        articles.extend(entries)
 
     articles = sorted(articles, key=lambda x: x[1].published_parsed, reverse=True)
 
@@ -29,10 +34,12 @@ def index():
     per_page = 10
     total_articles = len(articles)
     start = (page-1) * per_page
-    end = start + page
+    end = start + per_page
     paginated_articles = articles[start:end]
 
-    return render_template('index.html', articles=paginated_articles, page=page, total_page = total_articles // per_page + 1)
+    return render_template('index.html', articles=paginated_articles, page=page,
+                           total_pages = total_articles // per_page + 1)
+
 
 @app.route('/search')
 def search():
@@ -44,9 +51,10 @@ def search():
         entries = [(source, entry) for entry in parsed_feed.entries]
         articles.extend(entries)
 
-    results = [articles for article in articles if query.lower() in article[1].title.lower()]
+    results = [article for article in articles if query.lower() in article[1].title.lower()]
 
     return render_template('search_results.html', articles=results, query=query)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

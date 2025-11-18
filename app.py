9d0de458b2c2d995a5bdbfd4154fcc386ef554c2
Valentin Bretel <67462878+Vibetory@users.noreply.github.com>
2025-11-18 13:08:42 +0000

    bugged but all files created

--- app.py
+++ app.py
@@ -23,4 +23,30 @@ def index():
         parsed_feed = feedparser.parse(feed)
         entries = [(source, entry) for entry in parsed_feed.entries]
 
-    articles = sorted(articles, key=lambda x: x[1].published_gitparsed, reverse=True)
+    articles = sorted(articles, key=lambda x: x[1].published_parsed, reverse=True)
+
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    total_articles = len(articles)
+    start = (page-1) * per_page
+    end = start + page
+    paginated_articles = articles[start:end]
+
+    return render_template('index.html', articles=paginated_articles, page=page, total_page = total_articles // per_page + 1)
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+
+    articles = []
+    for source, feed in RSS_FEEDS.items():
+        parsed_feed = feedparser.parse(feed)
+        entries = [(source, entry) for entry in parsed_feed.entries]
+        articles.extend(entries)
+
+    results = [articles for article in articles if query.lower() in article[1].title.lower()]
+
+    return render_template('search_results.html', articles=results, query=query)
+
+if __name__ == '__main__':
+    app.run(debug=True)
